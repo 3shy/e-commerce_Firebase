a4ee34d29d65325343cbca6f5bf37ae8f92c6e34
Mohamed Ashry <am5a20109999@gmail.com>
2022-05-05 15:09:45 +0200

    Initial commit

---
+++ app/src/main/java/com/apps/ssdco/ecommerce/Interface/ItemClickListner.java
@@ -0,0 +1,7 @@
+package com.apps.ssdco.ecommerce.Interface;
+
+import android.view.View;
+
+public interface ItemClickListner {
+    void onClick(View view,int position,boolean islongClick);
+}
---
+++ app/src/main/java/com/apps/ssdco/ecommerce/Prevalent/Prevalent.java
@@ -0,0 +1,14 @@
+package com.apps.ssdco.ecommerce.Prevalent;
+
+import com.apps.ssdco.ecommerce.Model.Products;
+import com.apps.ssdco.ecommerce.Model.Users;
+
+public class Prevalent {
+
+    public static final   String UserPhoneKey ="UserPhone";
+    public static final String UserPasswordKey="UserPassword";
+    public static final String URL="https://ecommerce-59bee-default-rtdb.firebaseio.com/";
+    public static final String URL_STORAGE="https://ecommerce-59bee-default-rtdb.firebaseio.com/";
+    public static Users currentOnlineUser;
+    public static Products currentProduct;
+}
